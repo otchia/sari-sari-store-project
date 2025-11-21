af34303d2fb6326e805bc8d425ea41e06076536c
Joseph Emmanuel Magno <josephemmanuelmagno@gmail.com>
2025-11-21 21:19:34 +0800

    Fix Admin and Customer routes

--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -5,9 +5,5 @@ const router = express.Router();
 
 router.post("/register", registerAdmin);
 router.post("/login", loginAdmin);
-router.get("/", getAllAdmins);
-router.get("/:id", getAdminById);
-router.put("/:id", updateAdmin);
-router.delete("/:id", deleteAdmin);
 
 export default router;
